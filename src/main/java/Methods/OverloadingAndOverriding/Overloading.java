400cdd49ac106813628089dc59d5880912d9b1e6
dadhikary2560 <d.adhikary2560@gmail.com>
2026-09-12 23:10:46 +0530

    overloading and overriding

--- src/main/java/Methods/OverloadingAndOverriding/Overloading.java
+++ src/main/java/Methods/OverloadingAndOverriding/Overloading.java
@@ -1,4 +1,35 @@
 package Methods.OverloadingAndOverriding;
 
-public class Overloading {
+
+//same name but with different arguments
+public class Overloading
+{
+    public void m1()
+    {
+        System.out.println("Hi i am no argument method");
+    }
+    public void m1(int a)
+    {
+        System.out.println("HI i am parameterized method, value received: "+a);
+    }
+    public int m1(String a)
+    {
+        System.out.println("Hi "+a);
+        return 7;
+    }
+    //all these are instance methods
+
+    public static void m2()
+    {
+        System.out.println("Hi i am static method");
+    }
+
+    public static void main(String[] args) {
+        Overloading methodCall=new Overloading();
+        methodCall.m1();
+        methodCall.m1(7);
+        System.out.println(methodCall.m1("Debabrat"));
+
+        m2();
+    }
 }
---
+++ src/main/java/Methods/OverloadingAndOverriding/OverridingBase.java
@@ -0,0 +1,8 @@
+package Methods.OverloadingAndOverriding;
+
+public class OverridingBase {
+    public void m1()
+    {
+        System.out.println("Hi i am the base method");
+    }
+}
