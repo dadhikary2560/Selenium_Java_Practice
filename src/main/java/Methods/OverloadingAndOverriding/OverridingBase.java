a82f1dfbba4204fc793cc0dfd81e59f7061b22f0
dadhikary2560 <d.adhikary2560@gmail.com>
2026-09-14 20:24:23 +0530

    overriding base class

--- src/main/java/Methods/OverloadingAndOverriding/OverridingBase.java
+++ src/main/java/Methods/OverloadingAndOverriding/OverridingBase.java
@@ -4,5 +4,17 @@ public class OverridingBase {
     public void m1()
     {
         System.out.println("Hi i am the base method");
+        System.out.println("I am also the instance method");
+    }
+
+    //static method can not be overriden, so it gets hidden in the child class
+    public static void m2()
+    {
+        System.out.println("Hi i am the static method");
+    }
+
+    public static void main(String[] args) {
+        m2();
+        new OverridingBase().m1();
     }
 }
