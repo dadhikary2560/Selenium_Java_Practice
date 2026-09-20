0d33f123643a802664043cdef4fa508ab2733b12
dadhikary2560 <d.adhikary2560@gmail.com>
2026-09-20 12:12:08 +0530

    Reverse the given String

--- src/main/java/String/ReverseString.java
+++ src/main/java/String/ReverseString.java
@@ -1,4 +1,10 @@
 package String;
 
 public class ReverseString {
+    public static void main(String[] args) {
+        String s="Debabrat is my name";
+        System.out.println(s.length());
+        for (int i=s.length()-1;i>=0;i--)
+            System.out.print(s.charAt(i));
+    }
 }
