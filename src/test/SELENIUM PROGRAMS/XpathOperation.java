ca0084eacc52795f5fe70b47f81a800136cbf07b
dadhikary2560 <d.adhikary2560@gmail.com>
2026-09-25 09:52:38 +0530

    updated with orangeHRM and skillio methods

--- src/test/SELENIUM PROGRAMS/XpathOperation.java
+++ src/test/SELENIUM PROGRAMS/XpathOperation.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,6 +11,7 @@ import java.time.Duration;
 public class XpathOperation {
     static RemoteWebDriver driver=new ChromeDriver();
     static WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+    static Actions act=new Actions(driver);
 
     public static void skillio()
     {
@@ -23,9 +25,30 @@ public class XpathOperation {
         e.sendKeys("Debabrat Adhikary");
     }
 
+    public static void orangeHrm()
+    {
+        //first page, login page
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(@placeholder, \"Username\")]")));
+        driver.findElement(By.xpath("//input[contains(@placeholder, \"Username\")]")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[contains(@placeholder, \"Password\")]")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[contains(@class, 'oxd-button--main orangehrm-login-button')]")).click();
+
+        //dashboard, hover on Assign Leave icon
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@title,'Assign Leave')]")));
+        act.moveToElement(driver.findElement(By.xpath("//button[contains(@title,'Assign Leave')]")));
+        act.perform();          //action class can only perform with the perform method
+
+
+        //going to profile icon
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class, 'oxd-userdropdown-tab')]")));
+        driver.findElement(By.xpath("//span[contains(@class, 'oxd-userdropdown-tab')]")).click();
+    }
+
     public static void main(String[] args) {
         driver.manage().window().maximize();
-        skillio();
-        driver.quit();
+//        skillio();
+        orangeHrm();
+        //driver.quit();
     }
 }
