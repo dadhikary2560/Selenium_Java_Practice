ea2841910c701a99e7844e62f6be4bb95d3dd909
dadhikary2560 <d.adhikary2560@gmail.com>
2026-09-25 07:45:51 +0530

    updated with intro of method

--- src/test/SELENIUM PROGRAMS/XpathOperation.java
+++ src/test/SELENIUM PROGRAMS/XpathOperation.java
@@ -1,19 +1,19 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class XpathOperation {
-    public static void main(String[] args) {
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
+    static RemoteWebDriver driver=new ChromeDriver();
+    static WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    public static void skillio()
+    {
         driver.get("https://helloskillio.com/");
-        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(@class,\"dialog-close-button dialog-lightbox-close-button\")]")));
         driver.findElement(By.xpath("//a[contains(@class,\"dialog-close-button dialog-lightbox-close-button\")]")).click();
         driver.findElement(By.xpath("//a[contains(text(),\"Free Resources\")]")).click();
@@ -21,6 +21,11 @@ public class XpathOperation {
 
         WebElement e= driver.findElement(By.xpath("//label[contains(text(),\"Full Name\")]/following-sibling::input"));
         e.sendKeys("Debabrat Adhikary");
+    }
+
+    public static void main(String[] args) {
+        driver.manage().window().maximize();
+        skillio();
         driver.quit();
     }
 }
